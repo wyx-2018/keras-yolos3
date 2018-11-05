9f18b3affdc140efa2827948adb5df431d862027
wyx-2018 <43309265+wyx-2018@users.noreply.github.com>
2018-11-05 15:40:22 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,9 +7,10 @@ echo current dir is $PWD
 data_dir=/data/wyx-123456/yolov3
 config_dir=./yolov3.cfg
 weights=yolov3.weights
-weights_dir=/output
-cp $data_dir/$weights $weights_dir
+weights_dir=./yolov3.weights
 output_dir=model_data/yolo_weights.h5
+
+cp $data_dir/$weights ./
 python convert.py -w config_path=$config_dir weights_path=$weights_dir/$weights output_path=$output_dir
 # python yolo_video.py
 # python train.py
