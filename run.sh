cb3d919ce931b3f5bf501c36eda4a74e56f35ea1
wyx-2018 <43309265+wyx-2018@users.noreply.github.com>
2018-11-05 14:42:50 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
-python convert.py -w yolov3.cfg yolov3.weights model_data/yolov3.h5
+config_path=./yolov3.cfg
+weights_path=/data/wyx-123456/yolov3/yolov3.weights
+python convert.py -w --config_path --weights_path model_data/yolov3.h5
 # python yolo_video.py
 # python train.py
