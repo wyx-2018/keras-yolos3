bac53d159f2e61dcc0baa8960cd89d389dd86e47
wyx-2018 <43309265+wyx-2018@users.noreply.github.com>
2018-11-05 14:29:32 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-# python convert.py yolov3.cfg yolov3.weights model_data/yolov3.h5
+python convert.py -w yolov3.cfg yolov3.weights model_data/yolov3.h5
 # python yolo_video.py
-python train.py
+# python train.py
