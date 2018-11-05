953da769c6b846a1802e9eb83adbbb6b6e0b4e34
wyx-2018 <43309265+wyx-2018@users.noreply.github.com>
2018-11-05 21:02:18 +0800

    Update voc_annotation.py

--- voc_annotation.py
+++ voc_annotation.py
@@ -6,7 +6,7 @@ sets=[('2007', 'train'), ('2007', 'val'), ('2007', 'test')]
 
 # classes = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
 
-labels_path='F:/vehicle-detection/labels.txt'
+labels_path='labels.txt'
 with open(labels_path,'rb') as f:
     label_names = f.readlines()
     classes = [c.decode('utf-8').split(':')[0] for c in label_names]
