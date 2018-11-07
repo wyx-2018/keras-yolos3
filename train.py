25aa4ef213464f02338be46dcd714c5ce09c258d
wyx-2018 <43309265+wyx-2018@users.noreply.github.com>
2018-11-07 21:29:54 +0800

    Update train.py

--- train.py
+++ train.py
@@ -13,7 +13,7 @@ from yolo3.utils import get_random_data
  
 def _main():
     annotation_path = '/output/2007_train.txt'
-    log_dir = 'logs/000/'
+    log_dir = '/output'
     classes_path = 'model_data/voc_classes.txt'
     anchors_path = 'model_data/yolo_anchors.txt'
     class_names = get_classes(classes_path)
