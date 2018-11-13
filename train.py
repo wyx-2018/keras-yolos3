0738ac079a97d4af7832c44d09d0a78f01192dfb
wyx-2018 <43309265+wyx-2018@users.noreply.github.com>
2018-11-13 23:39:19 +0800

    Update train.py

--- train.py
+++ train.py
@@ -18,8 +18,8 @@ def _main():
     anchors_path = 'model_data/yolo_anchors.txt'
     class_names = get_classes(classes_path)
     anchors = get_anchors(anchors_path)
-    input_shape = (416,416) # multiple of 32, hw
-    model = create_model(input_shape, anchors, load_pretrained=True, len(class_names) )
+    input_shape = (432,320) # multiple of 32, hw
+    model = create_model(input_shape, anchors, len(class_names) )
     train(model, annotation_path, input_shape, anchors, len(class_names), log_dir=log_dir)
  
 def train(model, annotation_path, input_shape, anchors, num_classes, log_dir='logs/'):
@@ -41,7 +41,7 @@ def train(model, annotation_path, input_shape, anchors, num_classes, log_dir='lo
             steps_per_epoch=max(1, num_train//batch_size),
             validation_data=data_generator_wrap(lines[num_train:], batch_size, input_shape, anchors, num_classes),
             validation_steps=max(1, num_val//batch_size),
-            epochs=10,
+            epochs=9,
             initial_epoch=0)
     model.save_weights(log_dir + 'trained_weights.h5')
  
