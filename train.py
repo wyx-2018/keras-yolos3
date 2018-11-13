ccc7dbcedd09a8f9d8953a2c966e5858af6a07c7
wyx-2018 <43309265+wyx-2018@users.noreply.github.com>
2018-11-13 21:24:09 +0800

    Update train.py

--- train.py
+++ train.py
@@ -28,7 +28,7 @@ def train(model, annotation_path, input_shape, anchors, num_classes, log_dir='lo
     logging = TensorBoard(log_dir=log_dir)
     checkpoint = ModelCheckpoint(log_dir + "ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5",
         monitor='val_loss', save_weights_only=True, save_best_only=True, period=1)
-    batch_size = 50
+    batch_size = 10
     val_split = 0.1
     with open(annotation_path) as f:
         lines = f.readlines()
